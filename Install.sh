64acbca11bb6f1256d2b7dede1101036c3981bd8
NastyGamer <32023338+NastyGamer@users.noreply.github.com>
2018-07-30 20:20:27 +0200

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -2,4 +2,4 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install python-pygame -y
 wget https://raw.githubusercontent.com/NastyGamer/Robotik-2018-19/master/Ball-Detection.py
-
+sudo rm Install.sh

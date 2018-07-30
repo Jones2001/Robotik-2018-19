67f8e630b9174743b912fd7dda3f188e95d4f30e
NastyGamer <32023338+NastyGamer@users.noreply.github.com>
2018-07-30 17:09:30 +0200

    Add files via upload

--- Ball-Detection.py
+++ Ball-Detection.py
@@ -1,17 +1,14 @@
-#Required packages: time, threading, pygame (sudo apt-get update && sudo apt-get upgrade && sudo apt-get install python-pygame)
-#----Use GUI?----
+
 GUI = True
-#----End Use GUI?----
-#----Imports----
+
 import pygame.surface
 import pygame.camera
-import pygame.display
-import threading as th
-#----End Imports----
-#---Setup----
+
+
 pygame.init()
 pygame.camera.init()
 if(GUI):
+    import pygame.display
     Draw = pygame.Surface([160, 120])
     Display = pygame.display.set_mode((160, 120))
 cam = pygame.camera.Camera("/dev/video0",(160, 120))
@@ -19,76 +16,80 @@ cam.start()
 minOrange = (20, 70, 85)
 maxOrange = (30, 100, 100)
 Found = []
-#----End Setup----
-#----4 Threads (= TopLeft, TopRight, BottomL, BottomR) (= Faster)----
+
 def FindTL(Img):
     for y in range(0, 60, 5):
         for x in range(0, 80, 5):
             if(minOrange[0] <= Img.get_at((x, y)).hsva[0] and maxOrange[0] >= Img.get_at((x, y)).hsva[0]):  
-                if(GUI):
-                    Draw.set_at((x, y), maxOrange)
                 Found.append((x, y))
 
 def FindTR(Img):
     for y in range(0, 60, 5):
         for x in range(80, 160, 5):
             if(minOrange[0] <= Img.get_at((x, y)).hsva[0] and maxOrange[0] >= Img.get_at((x, y)).hsva[0]):
-                if(GUI):
-                    Draw.set_at((x, y), maxOrange)
                 Found.append((x, y))
 
 def FindBL(Img):
     for y in range(60 , 120, 5): 
         for x in range(0 , 80, 5):
             if(minOrange[0] <= Img.get_at((x, y)).hsva[0] and maxOrange[0] >= Img.get_at((x, y)).hsva[0]):  
-                if(GUI):
-                    Draw.set_at((x, y), maxOrange)
                 Found.append((x, y))
 
 def FindBR(Img):
     for y in range(60, 120, 5):
         for x in range(80, 160, 5):
             if(minOrange[0] <= Img.get_at((x, y)).hsva[0] and maxOrange[0] >= Img.get_at((x, y)).hsva[0]):
-                if(GUI):
-                    Draw.set_at((x, y), maxOrange)
                 Found.append((x, y))
 
-#----End 4 Threads----
-#----Clear The Pixels from the Img----
+
 def Clear():
     if(GUI):
         Draw.fill([0, 0, 0])
-    Found = []
-#----End Clear The Pixels from the Img----
-#----Show the Img----
+    
+
 def Show(Img):
     Display.blit(Img, (0, 0))
     pygame.display.update()
-#----End Show the Img----
-#----Start the 4 Threads----
+
 def Thread(Img):
     import threading as th
-    tTL = th.Thread(target = FindTL, args = (Img,))
-    tTR = th.Thread(target = FindTR, args = (Img,))
-    tBL = th.Thread(target = FindBL, args = (Img,))
-    tBR = th.Thread(target = FindBR, args = (Img,))
-    tTL.start()
-    tTR.start()
-    tBL.start()
-    tBR.start()
-#----End Start the 4 Threads----
-#----Take a picture and return it----
+    if(Img is not None):
+        tTL = th.Thread(target = FindTL, args = (Img,))
+        tTR = th.Thread(target = FindTR, args = (Img,))
+        tBL = th.Thread(target = FindBL, args = (Img,))
+        tBR = th.Thread(target = FindBR, args = (Img,))
+    else:
+        raise ValueError
+    try:
+        tTL.start()
+        tTR.start()
+        tBL.start()
+        tBR.start()
+    except th.ThreadError:
+        raise th.ThreadError
+
 def Pic():
-    return cam.get_image()
-#----End Take a picture and return it----
-#----The Actual Program----
+    if(cam.get_image() is not None):
+        return cam.get_image()
+    else:
+        raise ValueError 
+
+def Edit(Img, Found):
+    try:    
+        for i in range(len(Found)):
+            pygame.draw.line(Img, (209, 109, 33), Found[i], Found[i + 1])
+    except IndexError, ValueError:
+        pass
+
 if(__name__ == "__main__"):
     while True:
+        Found = []    
         if(GUI):
             Clear()
         Thread(Pic())
+        Edit(Draw, Found)
         if(GUI):
             Show(Draw)
         else:
             print(Found)
-#----End of The Actual Program----
+

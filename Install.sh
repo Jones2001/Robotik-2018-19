2e79176f5b8ead94068a36a5aab5833d1ef2082c
NastyGamer <32023338+NastyGamer@users.noreply.github.com>
2018-08-03 23:34:43 +0200

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -3,7 +3,7 @@ sudo apt-get upgrade -y
 sudo apt-get install python-pygame -y
 wget https://raw.githubusercontent.com/NastyGamer/Robotik-2018-19/master/Ball-Detection.py
 wget http://github.com/TinkerBoard/gpio_lib_python/archive/sbc/tinkerboard/python.zip GPIO_API_for_Python.zip
-sudo apt-get install python-dev
+sudo apt-get install python-dev -y
 unzip GPIO_API_for_Python.zip
 cd GPIO_API_for_Python/
 sudo python setup.py install
